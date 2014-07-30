41e7ecb4a56c3371d44d2d9e76ac840346dc6901
W. S. Wellington <bill@wellingtonnet.net>
2014-07-30 16:28:03 -0700

    can GET stuffs

--- skynet.py
+++ skynet.py
@@ -2,3 +2,18 @@
 #
 #
 
+# import necessary modules
+import sys
+import requests
+import json
+
+# import skynet modules
+from skynet_modules.config import *
+import skynet_modules.rest as rest
+
+def get_api(path):
+  data = rest.rest(('get',base_url+path,user,token))
+  
+# call main
+if __name__=='__main__':
+  get_api('configurations/')
