e02690c9ee8a9400171a2d9c595440ad799634a6
Caleb Hawkins <chawkins@fulcrum.net>
2016-02-23 15:07:14 -0800

    UserData functionality added (to be tested)

--- skytap/models/UserData.py
+++ skytap/models/UserData.py
@@ -1,5 +1,75 @@
+import json
+from skytap.models.SkytapResource import SkytapResource
 
 
-class UserData(object):
+class UserData(SkytapResource):
     def __init__(self, contents):
-        self.contents = contents
+        super(UserData, self).__init__(contents.rstrip())
+        self.id = 0
+
+    def clean_value(key, value):
+        default_time = 3
+        default_delay_min = 0
+        default_delay_max = 31
+
+        # Check value for shutdown_time and decide what to do with it
+        if key == "shutdown_time":
+            try:
+                if value == "-":
+                    # Permanent exclusion case
+                    pass
+                elif (int(value) < 0 or int(value) > 23):
+                    # Should be valid time
+                    value = str(default_time)
+            except ValueError:
+                # Not an int, revert to default
+                value = str(default_time)
+        else:
+            # shutdown_time does not exist, add it
+            value = str(default_time)
+
+        # Check value for shutdown_delay and decide what to do with it
+        if key == "shutdown_delay":
+            try:
+                if int(value) < default_delay_min:
+                    # Delay smaller than minimum, set to 0
+                    value = str(default_delay_min)
+                elif int(value) > default_delay_max:
+                    # Delay larger than maximum, set to 31
+                    value = str(default_delay_max)
+            except ValueError:
+                # Delay is not a number, change to 0
+                value = str(default_delay_min)
+        else:
+            # shutdown_delay does not exist, add it
+            value = str(default_delay_min)
+
+        # If this is valid, leave it alone. Otherwise, change the value to "-",
+        # which triggers the variable itself to be removed.
+        if key == "delete_environment":
+            try:
+                int(value)
+            except ValueError:
+                value = "error"
+
+        return value
+
+    def get_values(content):
+        content = content.split("\n")
+
+        values = {}
+
+        for i in content:
+            tokens = i.split()
+            # Check for valid YAML formatting in first and second tokens in each
+            # line, then add those values to dict.
+            if (tokens[0].endswith(":") and "#" not in tokens[0] and
+                    len(tokens) > 1 and "#" not in tokens[1]):
+                    values[tokens[0][:-1]] = clean_value(tokens[0][:-1],
+                                                         tokens[1])
+                    self.data[tokens[0][:-1]] = values[tokens[0][:-1]]
+
+        return values
+
+    def _calculate_custom_data(self):
+        values = get_values(self.content)
