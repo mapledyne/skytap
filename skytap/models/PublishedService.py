706caf9a42581e9292a295807999bd894f236620
calebh93 <hawkins.caleb93@gmail.com>
2016-03-10 13:28:43 -0800

    Fixed a bug which made service.delete() unusable

--- skytap/models/PublishedService.py
+++ skytap/models/PublishedService.py
@@ -1,9 +1,10 @@
 """Support for a published service resource in Skytap."""
+from skytap.framework.ApiClient import ApiClient  # noqa
+import skytap.framework.Utils as Utils  # noqa
 from skytap.models.SkytapResource import SkytapResource  # noqa
 
 
 class PublishedService(SkytapResource):
-    pass
 
     def delete(self):
         """Delete a service. Cannot be undone!"""
--- skytap/models/Vms.py
+++ skytap/models/Vms.py
@@ -15,11 +15,11 @@ class Vms(SkytapGroup):
         Args:
             vms_json (string): The JSON from Skytap API to build the list from.
             parent (Environment or Template): The parent object -
-                environment or template.
+                                              environment or template.
 
         """
         super(Vms, self).__init__()
-        self.load_list_from_json(vms_json, Vm)
+        self.load_list_from_json(vms_json, Vm, env)
         for v in self.data:
             self.data[v].data['url'] = ('/v2/configurations/' + str(env) +
                                         '/vms/' + str(self.data[v].id))
