f0960d855104da0323dd04a0305e3c97b60f6850
W. S. Wellington <bwellington@skytap.com>
2016-12-17 10:59:13 -0800

    made changes to match Michaels Public.py

--- skytap/models/PublicIP.py
+++ skytap/models/PublicIP.py
@@ -1,21 +1,10 @@
 """Support for a Public IP resource in Skytap."""
-from skytap.framework.ApiClient import ApiClient  # noqa
-import skytap.framework.Utils as Utils  # noqa
 from skytap.models.SkytapResource import SkytapResource  # noqa
 
 
 class PublicIP(SkytapResource):
     """One Public IP service object."""
 
-    def attach(self):
-        """Attach a public IP to a NIC."""
-
-
-    # def detach(self):
-    #     """Detach a public IP from a NIC."""
-    #
-    # def acquire(self):
-    #     """Acquire a public IP for use in a region."""
-    #
-    # def release (self):
-    #     """Release a public IP. Warning: Cannot be undone."""
+    def __init__(self, ip_json):
+        """Create one PublicIP object."""
+        super(PublicIP, self).__init__(ip_json)
