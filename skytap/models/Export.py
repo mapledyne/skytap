05469d3edffa188fd7fc9f5cfcec1fcb7f2acb89
W. S. Wellington <bwellington@skytap.com>
2016-12-22 19:50:31 -0800

    an intermediate commit

--- skytap/models/Export.py
+++ skytap/models/Export.py
@@ -1,6 +1,9 @@
 """Support for Skytap VM Exports."""
-from skytap.models.SkytapResource import SkytapResource
+import json
 
+from skytap.framework.ApiClient import ApiClient
+import skytap.framework.Utils as Utils
+from skytap.models.SkytapResource import SkytapResource
 
 class Export(SkytapResource):
     """One Skytap VM Export object."""
@@ -8,3 +11,14 @@ class Export(SkytapResource):
     def __init__(self, export_json):
         """Create one Export object."""
         super(Export, self).__init__(export_json)
+
+    def delete_export_job(self, export_job):
+        if type (job) is not int:
+            raise TypeError('Export job must be an int.')
+
+    Utils.info('Deleting job ' +str(export_job) +
+                ' from queue.')
+    api = ApiClient()
+    api.rest(self.url + '/v2/exports/' + str(export_job),
+            {},
+            'DELETE')
